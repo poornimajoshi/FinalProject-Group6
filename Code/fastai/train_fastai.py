9b965e59900b2ecfee46325d6c20bca970981b36
chirag-jhamb <jhamb.chirag@gmail.com>
2019-12-03 16:43:02 -0500

    Added individual report and fastai code

--- Code/fastai/train_fastai.py
+++ Code/fastai/train_fastai.py
@@ -1,31 +1,51 @@
-import numpy as np
-import cv2, json, torch
-import torch.utils.data
-from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
-main_path = "/home/ubuntu/exam_data/train/"
+from os import listdir
+from os.path import isfile, join
+import pickle
+from fastai.vision import *
 
-RESIZE_TO = 50
-def crop_segment(img, json_attribute):
-    # im = cv2.imread(path + '.png')
-    x,y = json_attribute["bounding_box"]["minimum"]["r"],json_attribute["bounding_box"]["minimum"]["c"]
-    x2,y2 = json_attribute["bounding_box"]["maximum"]["r"],json_attribute["bounding_box"]["maximum"]["c"]
-    return np.array(cv2.resize(img[x:x2,y:y2], (RESIZE_TO, RESIZE_TO)))
-#     cv2.imwrite("roi3.jpg", roi)
-train_data = []
-train_labels = []
-for j in range(0,4): #1328)
-    try:
-        img_path = main_path + "cells_" + str(j) + ".png"
-        json_path = main_path + "cells_" + str(j) + ".json"
-        im = cv2.imread(img_path)
-        with open(json_path) as f:
-            d = json.load(f)
-        for i in range(len(d)):
-            train_data.append([crop_segment(im, d[i]), d[i]["category"]])
-#             train_data.append([crop_segment(im, d[i])])
-#             train_labels.append(d[i]["category"])
-    except FileNotFoundError:
-        print("FileNotFoundError",img_path)
-print(train_data[0])
-# trainloader = torch.utils.data.DataLoader(train_data, shuffle=True)
+def predict(img_path):
+        img = open_image(img_path)
+        p = learn.predict(img)
+        lbl = categorical[img_path.split("/")[-2]]
+        return [categorical[str(p[0])], p[2], lbl]
+
+path = "/home/ubuntu/data/project_data/cropped2/train/"
+data = ImageDataBunch.from_folder(path, train=".", valid_pct=0.2,
+        ds_tfms=get_transforms(), size=224, num_workers=4).normalize(imagenet_stats)
+
+print(data.classes, data.c, len(data.train_ds), len(data.valid_ds))
+
+learn = cnn_learner(data, models.resnet50, metrics=accuracy)
+learn.model_dir='/home/ubuntu/'
+learn.fit_one_cycle(4)
+learn.save('stage-1')
+learn.unfreeze()
+learn.lr_find()
+learn.fit_one_cycle(10, max_lr=slice(3e-6,3e-4))
+
+
+learn = cnn_learner(data, models.densenet101, metrics=accuracy)
+learn.model_dir='/home/ubuntu/'
+learn.fit_one_cycle(14, max_lr=slice(3e-6,3e-4))
+
+mypath = "/home/ubuntu/data/project_data/cropped2/train/real/"
+onlyfiles = []
+onlyfiles.extend([mypath + f for f in listdir(mypath) if isfile(join(mypath, f))])
+mypath = "/home/ubuntu/data/project_data/cropped2/train/fake/"
+onlyfiles.extend([mypath + f for f in listdir(mypath) if isfile(join(mypath, f))])
+onlyfiles = sorted(onlyfiles)
+prediction = []
+prediction_prob = []
+categorical = {"real":1,
+               "fake":0}
+acc = []
+result = []
+for i in onlyfiles:
+    ans = predict(i)
+    if ans[0]==categorical[i.split("/")[-2]]:
+            acc.append(1)
+    else:
+            acc.append(0)
+    result.append(ans)
+df = pd.DataFrame(result, columns=["Prediction", "Logits", "TrueLabels"])
+df.to_csv("densenet121_train_chirag.csv", index=False)
